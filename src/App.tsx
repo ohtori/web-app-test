e5ef73ab7feb98000a4ed5357390ddb01d327142
VIMPELCOM_MAIN\IMerzlyakov <IMerzlyakov@izk.beeline.com>
2022-08-24 16:22:30 +0400

    Display tgbot/getUpdates results

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-
+  const [serverData, setServerData] = useState({ok: null, result: []});
   // @ts-ignore:next-line
-  useEffect(() => console.log(window.Telegram.WebApp));
   useEffect(() => {
     fetch('https://api.telegram.org/bot5705683065:AAEKv_FvBf83a9m4JmwAJM_qwEvRXcJ6rf4/getUpdates')
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => setServerData(data))
       .catch(e => e.message)
   });
 
@@ -22,10 +21,7 @@ function App() {
         <div className='container'>
           <h2>Messages List</h2>
           <ul className='messages-list'>
-            <li>Hello!</li>
-            <li>Hi</li>
-            <li>Isabella, Princess of Asturias (2 October 1470 – 23 August 1498) was the eldest daughter and heir presumptive of King Ferdinand II of Aragon and Queen Isabella I of Castile. She was Queen of Portugal as the wife of King Manuel I from 30 September 1497 until her death the following year. </li>
-            <li>William Samoei Arap Ruto (born 21 December 1966) is a Kenyan politician who is the president-elect of Kenya.[1] He has served as the deputy president of Kenya since 2013.[2][3][4] In the 2013 presidential election, he was elected the Deputy President alongside President Uhuru Kenyatta under the Jubilee Alliance ticket.</li>
+            {serverData.result.map((message: {message: any}) => <li>{JSON.stringify(message.message, null, 2)}</li>)}
           </ul>
         </div>
       </main>

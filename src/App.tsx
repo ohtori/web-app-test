59fc58fe888b1e67c0f9cbee930a697104a2c95f
VIMPELCOM_MAIN\IMerzlyakov <IMerzlyakov@izk.beeline.com>
2022-08-28 01:25:03 +0400

    Add hide content feature if it open not by tg

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+
+interface CheckDataStorage {
+  userID: string
+  colorScheme: string
+  backgroundColor: string
+}
+
+// @ts-ignore:next-line
+const tg: any = window.Telegram.WebApp;
+
+const checkDataStorage: CheckDataStorage = {} as CheckDataStorage;
+Object.defineProperty(checkDataStorage, 'userID', {
+  configurable: false, writable: false, enumerable: false, value: tg.initDataUnsafe?.user?.id
+});
+Object.defineProperty(checkDataStorage, 'colorScheme', {
+  configurable: false, writable: false, enumerable: false, value: tg.initDataUnsafe?.hash
+});
+Object.defineProperty(checkDataStorage, 'backgroundColor', {
+  configurable: false, writable: false, enumerable: false, value: tg.backgroundColor
+});
 
 function App() {
-  useEffect(() => {
-      // @ts-ignore:next-line
-      console.log(window.Telegram.WebApp);
-      
-  });
-
   return (
-    <>
-      <header className="header">
-        <div className='container'>
-          <h1>Telegram WebApp test</h1>
-        </div>
-      </header>
-      <main className='main'>
-        <div className='container'>
-          <h2>Messages List</h2>
-          <ul className='messages-list'>
-            <li>
-              {
-                // @ts-ignore:next-line
-                <pre>{JSON.stringify(window.Telegram.WebApp, null, '\t')}</pre>
-              }
-            </li>
-          </ul>
-        </div>
-      </main>
-    </>
+    (checkDataStorage.userID && checkDataStorage.colorScheme && checkDataStorage.backgroundColor) ? 
+      <>
+        <header className="header">
+          <div className='container'>
+            <h1>Telegram WebApp test</h1>
+          </div>
+        </header>
+        <main className='main'>
+          <div className='container'>
+            <h2>Messages List</h2>
+            <ul className='messages-list'>
+              <li>
+                {
+                  // @ts-ignore:next-line
+                  <pre>{JSON.stringify(tg, null, '\t')}</pre>
+                }
+              </li>
+            </ul>
+          </div>
+        </main>
+      </>
+    : null
   );
 }
 
